74a15ee4231415a5a65b5cbe0fd3afba0b45dd8d
WhatIsLove48 <trrunovva@gmail.com>
2020-07-17 21:53:46 +0300

    Finish Threads 12.07.20

---
+++ src/Chapter_14_Generalization/OverridingMethodGenClass/MainClass.java
@@ -0,0 +1,36 @@
+package Chapter_14_Generalization.OverridingMethodGenClass;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Gen<Integer> ob = new Gen<Integer>(789);
+        Gen2<Integer> ob2 = new Gen2<Integer>(79);
+        Gen2<String> str = new Gen2<String>("String");
+
+        System.out.println(ob.getOb());
+        System.out.println(ob2.getOb());
+        System.out.println(str.getOb());
+
+    }
+}
+class Gen<T>{
+    T ob;
+
+    public Gen(T ob) {
+        this.ob = ob;
+    }
+    T getOb(){
+        System.out.println("Get2's getob():");
+        return ob;
+    }
+}
+class Gen2<T> extends Gen<T>{
+    public Gen2(T ob) {
+        super(ob);
+    }
+
+    @Override
+    T getOb(){
+        System.out.println("Get2's getob():");
+        return ob;
+    }
+}
---
+++ src/Chapter_14_Generalization/TypeInterfaceGen.java
@@ -0,0 +1,16 @@
+package Chapter_14_Generalization;
+
+public class TypeInterfaceGen {
+    public static void main(String[] args) {
+
+    }
+}
+class MyClass<T,V>{
+    T ob1;
+    V ob2;
+
+    public MyClass(T ob1, V ob2) {
+        this.ob1 = ob1;
+        this.ob2 = ob2;
+    }
+}
