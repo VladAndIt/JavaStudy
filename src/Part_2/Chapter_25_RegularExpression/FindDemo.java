1d2b58247a4bf1e85c51d0b9ef4006fed66e469d
Vlad <trrunovva@gmail.com>
2020-08-12 20:34:12 +0300

    Patterns

---
+++ src/Part_2/Chapter_25_RegularExpression/FindDemo.java
@@ -0,0 +1,21 @@
+package Part_2.Chapter_25_RegularExpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FindDemo {
+    public static void main(String[] args) {
+        Pattern pat = Pattern.compile("Java");
+        Matcher mat = pat.matcher("Java SE");
+        System.out.println("Looking for Java in Java SE");
+
+        if (mat.find()) System.out.println("subsequence found");
+        else System.out.println("No Match");
+
+        Pattern pat1 = Pattern.compile("test");
+        Matcher mat1 = pat1.matcher("test 1 2 3 test adakksajfgkjeejdw  test");
+        while (mat1.find()) {
+            System.out.println("Sequence 'test' is found " + mat1.start());
+        }
+    }
+}
---
+++ src/Part_2/Chapter_25_RegularExpression/PatternMatchingBasic.java
@@ -0,0 +1,30 @@
+package Part_2.Chapter_25_RegularExpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatchingBasic {
+    public static void main(String[] args) {
+        Pattern pat;
+        Matcher mat;
+        boolean found;
+
+        pat = Pattern.compile("Java");
+        mat = pat.matcher("Java");
+        found = mat.matches();
+
+        System.out.println("Testing Java against Java.");
+        if(found) System.out.println("Matches");
+        else System.out.println("No Match");
+
+        System.out.println();
+
+        System.out.println("Testing Java against Java SE.");
+        pat = Pattern.compile("Java SE");
+        mat = pat.matcher("Java SE");
+        found = mat.matches();
+        if (found) {
+            System.out.println("Matches!");
+        } else System.out.println("No Match!");
+    }
+}
